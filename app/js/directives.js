a56bb092db2dcb93371421310e21cab05a82ba3b
Arıl Bozoluk <arilbozoluk@hotmail.com>
2013-12-26 23:36:58 +0200

    fixed markdown

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,19 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+  .directive('appVersion', ['version', function(version) {
+      return function(scope, elm, attrs) {
+          elm.text(version);
+      }
+  }])
+    .directive('markdown', function () {
+        var converter = new Showdown.converter();
+        return {
+            restrict: 'C',
+            link: function (scope, element, attrs) {
+                var htmlText = converter.makeHtml(element.text());
+                element.html(htmlText);
+            }
+        }
+    });
